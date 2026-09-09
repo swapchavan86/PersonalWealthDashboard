c9c9902e8458049d711619da25adcee1af4fd030
swapnil chavan <swapnil740@gmail.com>
2026-09-09 23:04:28 +0530

    PW-DB-001: Register persistence in API host

--- src/PersonalWealth.Api/Program.cs
+++ src/PersonalWealth.Api/Program.cs
@@ -1,6 +1,9 @@
+using PersonalWealth.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 
