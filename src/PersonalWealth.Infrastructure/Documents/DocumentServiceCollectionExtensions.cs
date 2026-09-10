c461fbc7da02e73246ad200f2b244a42fe15b2d4
swapnil chavan <swapnil740@gmail.com>
2026-09-10 19:05:50 +0530

    feat(investments): register phase 6 services

--- src/PersonalWealth.Infrastructure/Documents/DocumentServiceCollectionExtensions.cs
+++ src/PersonalWealth.Infrastructure/Documents/DocumentServiceCollectionExtensions.cs
@@ -17,34 +17,11 @@ public static class DocumentServiceCollectionExtensions
 {
     public static IServiceCollection AddDocumentServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddOptions<DocumentStorageOptions>()
-            .Bind(configuration.GetSection(DocumentStorageOptions.SectionName))
-            .Validate(options => !string.IsNullOrWhiteSpace(options.RootPath));
-
-        services.AddSingleton<DocumentFolderProvider>();
-        services.AddSingleton<IDocumentScanner, FileSystemDocumentScanner>();
-        services.AddSingleton<IDocumentHasher, DocumentHasher>();
-        services.AddSingleton<IDocumentTemplateSelector, DocumentTemplateSelector>();
-        services.AddSingleton<ITransactionBusinessValidator, TransactionBusinessValidator>();
-        services.AddSingleton<IImportStagingStore, InMemoryImportStagingStore>();
-        services.AddSingleton<ImportLifecycleService>();
-        services.AddSingleton<IBankDocumentParser, SampleBankCsvParser>();
-        services.AddScoped<IBankAccountRepository, EfBankAccountRepository>();
-        services.AddScoped<IBankTransactionRepository, EfBankTransactionRepository>();
-        services.AddScoped<ITransactionNormalizer, TransactionNormalizer>();
-        services.AddScoped<ITransactionCategorizer, RuleBasedTransactionCategorizer>();
-        services.AddScoped<ITransferDetector, TransferDetector>();
-        services.AddScoped<IStatementReconciler, StatementReconciler>();
-        services.AddScoped<IBankImportService, EfBankImportService>();
-        services.AddScoped<IFirstBankImportWorkflow, FirstBankImportWorkflow>();
-        services.AddScoped<IExpenseCategoryRepository, EfExpenseCategoryRepository>();
-        services.AddScoped<IExpenseRepository, EfExpenseRepository>();
-        services.AddScoped<IRecurringExpenseRepository, EfRecurringExpenseRepository>();
-        services.AddScoped<IExpenseReportingRepository, EfExpenseReportingRepository>();
-        services.AddScoped<IExpenseService, ExpenseService>();
-        services.AddScoped<IInvestmentPortfolioEngine, InvestmentPortfolioEngine>();
-        services.AddScoped<IInvestmentValuationService, InvestmentValuationService>();
-
+        services.AddOptions<DocumentStorageOptions>().Bind(configuration.GetSection(DocumentStorageOptions.SectionName)).Validate(options => !string.IsNullOrWhiteSpace(options.RootPath));
+        services.AddSingleton<DocumentFolderProvider>(); services.AddSingleton<IDocumentScanner, FileSystemDocumentScanner>(); services.AddSingleton<IDocumentHasher, DocumentHasher>(); services.AddSingleton<IDocumentTemplateSelector, DocumentTemplateSelector>(); services.AddSingleton<ITransactionBusinessValidator, TransactionBusinessValidator>(); services.AddSingleton<IImportStagingStore, InMemoryImportStagingStore>(); services.AddSingleton<ImportLifecycleService>(); services.AddSingleton<IBankDocumentParser, SampleBankCsvParser>();
+        services.AddScoped<IBankAccountRepository, EfBankAccountRepository>(); services.AddScoped<IBankTransactionRepository, EfBankTransactionRepository>(); services.AddScoped<ITransactionNormalizer, TransactionNormalizer>(); services.AddScoped<ITransactionCategorizer, RuleBasedTransactionCategorizer>(); services.AddScoped<ITransferDetector, TransferDetector>(); services.AddScoped<IStatementReconciler, StatementReconciler>(); services.AddScoped<IBankImportService, EfBankImportService>(); services.AddScoped<IFirstBankImportWorkflow, FirstBankImportWorkflow>();
+        services.AddScoped<IExpenseCategoryRepository, EfExpenseCategoryRepository>(); services.AddScoped<IExpenseRepository, EfExpenseRepository>(); services.AddScoped<IRecurringExpenseRepository, EfRecurringExpenseRepository>(); services.AddScoped<IExpenseReportingRepository, EfExpenseReportingRepository>(); services.AddScoped<IExpenseService, ExpenseService>();
+        services.AddScoped<IInvestmentPortfolioEngine, InvestmentPortfolioEngine>(); services.AddScoped<IInvestmentValuationService, InvestmentValuationService>(); services.AddScoped<IInvestmentReconciliationService, InvestmentReconciliationService>(); services.AddScoped<IInvestmentImportService, InvestmentImportService>();
         return services;
     }
 }
