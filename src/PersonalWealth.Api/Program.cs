593ec8d0e3cccfd554e342a3b666598b2d2ae426
swapnil chavan <swapnil740@gmail.com>
2026-09-10 11:40:53 +0530

    feat: register document services in API

--- src/PersonalWealth.Api/Program.cs
+++ src/PersonalWealth.Api/Program.cs
@@ -1,9 +1,11 @@
+using PersonalWealth.Infrastructure.Documents;
 using PersonalWealth.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddDocumentServices(builder.Configuration);
 
 var app = builder.Build();
 
